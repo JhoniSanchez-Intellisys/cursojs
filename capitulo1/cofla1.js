dd4a0b968914ec2d395ea853f146a55cb18041ac
JhoniSanchez-Intellisys <jsanchezs@intellisys.com.do>
2024-03-07 08:52:11 -0400

    Finalizado ejercicio 1.1

--- capitulo1/cofla1.js
+++ capitulo1/cofla1.js
@@ -1,32 +1,25 @@
-const menu =  {
-    "palito de helado de agua": 0.6, 
+const menu = {
+    "palito de helado de agua": 0.6,
     "palito de helado de crema": 1,
     "Bombom helado marca heladix": 1.6,
-    "bombom helado marca heladovich": 1.7, 
-    "bombom helado marca helardo": 1.8, 
-    "potecito de helado con confetis": 2.9,
-    "pote de 1./4": 2.9
+    "bombom helado marca heladovich": 1.7,
+    "bombom helado marca helardo": 1.8,
+    "potecito de helado con confetis o pote de 1./4": 2.9,
+    "Limite": 1000000
 }
 
 const arra = Object.entries(menu)
-console.log(arra)
 
 const aa = (dd) => {
-    let cofra = prompt("Monto condecimales")
+    let cofra = prompt("Ingresar monto disponible para comprar")
     let monto = parseFloat(cofra)
-    console.log(monto)
-
-    for (let i = 0; i < arra.length+1; i++) {
-        console.log(monto >= arra[i][1] && monto < arra[i+1][1])
-        if (monto >= arra[i][1] && monto < arra[i+1][1]) {
-           console.log(monto > arra[i][1], monto, arra[i][1]) 
-            alert(`${dd} Monto es ${monto.toFixed(2)} ,precio es ${arra[i][1]},  devuleta es ${parseFloat(monto - arra[i][1]).toFixed(2)}`)
+    for (let i = 0; i < arra.length; i++) {
+        if (monto >= arra[i][1] && monto < arra[i + 1][1]) {
+            alert(`${dd} Para el monto ${monto.toFixed(2)}, el eleado mas caro a comprar es ${arra[i][0]}, por el precio de ${arra[i][1]},  devuleta es ${parseFloat(monto - arra[i][1]).toFixed(2)}`)
             return
         }
     }
-    alert(`Monto es ${monto}, precio maximo es 7,  no hay capacidad de pago`)
-
+    alert(`Para el monto ${monto}, no hay elados disponibles`)
 }
 
-aa("Jhoni")
-2
+aa("Cliente:_____,")
