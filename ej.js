a98d5871e8c51793e5f3a5d03b8b98c66e3791d3
JhoniSanchez-Intellisys <jsanchezs@intellisys.com.do>
2024-03-08 16:22:09 -0400

    Avance Js Cap 2

--- ej.js
+++ ej.js
@@ -27,17 +27,56 @@
 
 // aa()
 
-const aa = () => {
+// const aa = () => {
 
-  let i = 0;
-  let a = [1, 3, 4, 6, 7, 8, 99];
-  while (i < 100) {
+//   let i = 0;
+//   let a = [1, 3, 4, 6, 7, 8, 99];
+//   while (i < 100) {
+//     i++;
+//     if (i === 1 || i === 99|| i === 98 ) {
+//       continue;
+//     }
+//     console.log(i);
+//   }
+// };
+
+// aa();
+
+// const aa = () => {
+//   let i = 0;
+
+//   while (i < 100) {
+//     i++;
+//     if (i == 6) {
+//       return;
+//     }
+//     console.log(i);
+//   }
+//    console.log("Finalizado");
+// };
+
+// aa();
+
+// const aa = ["casa", "perro", "cama", "calle", "botella", "vino"];
+// const bb = [...aa, "perro", "cama", "calle", "botella", "vino"];
+// for (ff in aa) {
+//   console.log(ff);
+// }
+
+// for (ff of bb) {
+//   console.log(ff);
+// }
+
+const aa = [11, 55, 88, 445, 475, 96658, 524, 54, 658, 254, 69, 5];
+let i = 0;
+
+otro: while (i < aa.length) {
+  
+  console.log("Otro", aa[i]);
+i++;
+  while (i < aa.length) {
     i++;
-    if (i === 1 || i === 99|| i === 98 ) {
-      continue;
-    }
-    console.log(i);
+    console.log(aa[i]);
+    if (aa[i] == 475) continue otro;
   }
-};
-
-aa();
+}
